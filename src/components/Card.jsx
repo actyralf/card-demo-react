2b3cd124e247d1dc3dc59416821d0019d498f72e
Ralf Siewert <ralf@actyvy.st>
2023-08-29 16:06:54 +0200

    add favorite button

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,18 @@
 import "./Card.css";
+import { useState } from "react";
 
 export function Card(props) {
+  const [isFavorite, setIsFavorite] = useState(false);
+  function handleFavoriteClick() {
+    setIsFavorite(!isFavorite);
+  }
   return (
     <div className="card">
+      <img
+        onClick={handleFavoriteClick}
+        className="favorite-icon"
+        src={isFavorite ? "heart-filled.png" : "heart-outlined.png"}
+      />
       <img src={props.image} width="70%" />
       <h2>{props.name}</h2>
       <p>{props.job}</p>
