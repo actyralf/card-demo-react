10c1538b351a422711f11ba39164ff2d0a22e74b
Ralf Siewert <ralf@actyvy.st>
2023-08-31 09:24:42 +0200

    array functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
+import { useState } from "react";
 import "./App.css";
 import { Card } from "./components/Card";
 import { users } from "./data/users";
 
 function App() {
+  const [selectedFilter, setSelectedFilter] = useState("All");
+
+  let listItems;
+  if (selectedFilter === "All") {
+    listItems = users;
+  } else if (selectedFilter === "Women") {
+    listItems = users.filter((user) => user.gender === "female");
+  } else if (selectedFilter === "Men") {
+    listItems = users.filter((user) => user.gender === "male");
+  } else if (selectedFilter === "By name") {
+    listItems = users
+      .slice()
+      .sort((a, b) => (a.name.last > b.name.last ? 1 : -1));
+  } else if (selectedFilter === "By age") {
+    listItems = users.slice().sort((a, b) => (a.dob.age > b.dob.age ? 1 : -1));
+    listItems = users.sort((a, b) => (a.dob.age > b.dob.age ? 1 : -1));
+  }
+
   return (
     <>
       <header>
@@ -12,19 +31,73 @@ function App() {
         <section className="control-panel">
           <section className="control-panel__section">
             <h3>Filter</h3>
-            <button>All</button>
-            <button>Women</button>
-            <button>Men</button>
+            <button
+              style={{
+                backgroundColor: selectedFilter === "All" ? "gray" : undefined,
+                color: selectedFilter === "All" ? "white" : undefined,
+              }}
+              onClick={() => {
+                console.log("All clicked");
+                setSelectedFilter("All");
+              }}
+            >
+              All
+            </button>
+            <button
+              style={{
+                backgroundColor:
+                  selectedFilter === "Women" ? "gray" : undefined,
+                color: selectedFilter === "Women" ? "white" : undefined,
+              }}
+              onClick={() => {
+                setSelectedFilter("Women");
+              }}
+            >
+              Women
+            </button>
+            <button
+              style={{
+                backgroundColor: selectedFilter === "Men" ? "gray" : undefined,
+                color: selectedFilter === "Men" ? "white" : undefined,
+              }}
+              onClick={() => {
+                setSelectedFilter("Men");
+              }}
+            >
+              Men
+            </button>
           </section>
           <section className="control-panel__section">
             <h3>Sort</h3>
-            <button>By name</button>
-            <button>By age</button>
+            <button
+              style={{
+                backgroundColor:
+                  selectedFilter === "By name" ? "gray" : undefined,
+                color: selectedFilter === "By name" ? "white" : undefined,
+              }}
+              onClick={() => {
+                setSelectedFilter("By name");
+              }}
+            >
+              By name
+            </button>
+            <button
+              style={{
+                backgroundColor:
+                  selectedFilter === "By age" ? "gray" : undefined,
+                color: selectedFilter === "By age" ? "white" : undefined,
+              }}
+              onClick={() => {
+                setSelectedFilter("By age");
+              }}
+            >
+              By age
+            </button>
           </section>
         </section>
 
         <section className="card-list">
-          {users.map((user) => {
+          {listItems.map((user) => {
             return <Card key={user.email} user={user} />;
           })}
         </section>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,6 +7,7 @@ export function Card(props) {
       <img src={user.picture.medium} width="70%" />
       <h2>{`${user.name.first} ${user.name.last}`}</h2>
       <p>{user.location.country}</p>
+      <p>{user.dob.age} years</p>
     </div>
   );
 }
